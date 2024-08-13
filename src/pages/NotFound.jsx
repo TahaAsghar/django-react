9a1e17e2954b921e528fdbd7c2e31f36a530b16e
TahaAsghar <realasticaproch@gmail.com>
2024-08-13 17:47:41 +0500

    Pushing all code

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound(){
+    return <div>
+        <h3>Not found</h3>
+        <p>The page you are looking may not exist anymore</p>
+    </div>
+}
+
+export default NotFound
---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import Form from "../components/form.jsx";
+
+function Register(){
+    return <Form
+    route="/api/user/register/"
+    method="register"
+    />
+}
+
+export default Register
